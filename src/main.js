9e5cde44dd69a39aad95d92e4cf00e9edbd2430a
hectortllo <hectortllo@gmail.com>
2020-03-10 23:44:03 -0600

    Se agregaron algunos comentarios y Tailwind para trabajar el CSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+//Importar la librería (es una librería, porque no tiene extensión)
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 
+//La instancia de Vue
 new Vue({
   render: h => h(App)
 }).$mount("#app");
