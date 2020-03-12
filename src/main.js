413bae3ad413aea5d80e37274d9626f3eada7bc6
hectortllo <hectortllo@gmail.com>
2020-03-11 21:42:56 -0600

    1. Se añadió la librería  vue-router para realizar una SPA. 2. Se crearon todas las vistas para la SPA. 3. Se añadieron todas las funcionalidades a todas las páginas que formarán parte del proyecto

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
+//Importando el router para utilizar la funcionalidad SPA
+import router from '@/router'
+
 Vue.config.productionTip = false;
 
 //La instancia de Vue
 new Vue({
+  router: router,
   render: h => h(App)
 }).$mount("#app");
